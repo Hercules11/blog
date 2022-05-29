9f23ab6b2868377fea2ac9d23b1f57540230d877
WXC <43318823+Hercules11@users.noreply.github.com>
2022-05-30 00:24:45 +0800

    update

--- scripts/openNewFile.js
+++ scripts/openNewFile.js
@@ -2,5 +2,5 @@ var { spawn } = require('child_process');
 
 hexo.on('new', function (data) {
     // spawn("D:\\Typora\\Typora.exe", [data.path]);
-    spawn("D:/Typora/Typora.exe", [data.path]);
+    spawn("D:\\Program Files\\Typora\\Typora.exe", [data.path]);
 })
